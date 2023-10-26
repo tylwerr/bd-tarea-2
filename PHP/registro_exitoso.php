eaee74af09c79cad133c0c232e2c028c0ff23693
Daniel Maturana <matucristi@gmail.com>
2023-10-25 23:47:52 -0300

    registro_exitoso actualizado

--- PHP/registro_exitoso.php
+++ PHP/registro_exitoso.php
@@ -4,9 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro Exitoso</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <style>
+        div.card{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-49%, -49%);
+        }
+
+        div.alert{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-178%, -179%);
+        }
+    </style>
+
 </head>
 <body>
-    <h2>¡Registro Exitoso!</h2>
-    <p>Tu usuario se ha registrado correctamente. Ahora puedes <a href="login.php">iniciar sesión</a>.</p>
+    <div class="alert alert-primary" role="alert">
+    ¡Registro Exitoso!
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            ¡Tu usuario se ha registrado correctamente! Ahora puedes <button type="button" class="btn btn-primary" onclick="location.href='login.php'">Iniciar Sesión</button>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
 </html>
