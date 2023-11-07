335c2a14d64cae7379905e2cd4984cc8f9c3ff38
Daniel Maturana <matucristi@gmail.com>
2023-11-07 12:21:06 -0300

    top bar en favorito

--- PHP/favoritos.php
+++ PHP/favoritos.php
@@ -1,6 +1,5 @@
 <?php
-include_once("config.php");
-session_start();
+include("top_bar.php");
 $id_receta = '';
 $mensaje = '';
 
@@ -32,7 +31,6 @@ if (isset($_SESSION['email'])) {
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Favoritos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <style>
         .col-md-4 {
@@ -122,8 +120,6 @@ if (isset($_SESSION['email'])) {
             <?php } } ?>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 </html>
