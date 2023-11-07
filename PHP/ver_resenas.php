a9612d8601c75bfe9304a4eae04e38bd4d0d3405
Daniel Maturana <matucristi@gmail.com>
2023-11-07 12:26:40 -0300

    top bar en resenas y votar

--- PHP/ver_resenas.php
+++ PHP/ver_resenas.php
@@ -1,11 +1,8 @@
 <?php
-include_once("config.php");
-session_start();
+include("top_bar.php");
 $mensaje = '';
 
 if (isset($_SESSION['email'])) {
-    $email = $_SESSION['email'];
-    $conn = Cconexion::ConexionBD();
 
     $sql = "SELECT vr.id_user, vr.id_receta, vr.calificacion, vr.comentario, vr.fecha_resena, r.nombre_receta
             FROM vista_resenas vr
@@ -32,7 +29,6 @@ if (isset($_SESSION['email'])) {
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tus reseñas</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <style>
         body {
@@ -40,20 +36,7 @@ if (isset($_SESSION['email'])) {
             background-color: #f3f3f3;
             margin: 0;
             padding: 0;
-        }
-        .top-bar {
-            background-color: #074469;
-            color: #fff;
-            padding: 10px;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-        }
 
-        .right-image {
-            width: 300px;
-            height: 60px;
-            margin-right : 10px;
         }
 
         .btn-back {
@@ -92,9 +75,6 @@ if (isset($_SESSION['email'])) {
 </head>
 
 <body>
-    <div class="top-bar">
-        <img class="right-image" src="//aula.usm.cl/pluginfile.php/1/theme_moove/logo/1697696553/marca-color.png" alt="USM04">
-    </div>
     <div style="margin: 10px;">
         <a href="perfil.php" class="btn btn-back">Atrás</a>
     </div>
@@ -148,11 +128,6 @@ if (isset($_SESSION['email'])) {
                 </div>
         <?php } }?>
     </div>
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 </html>
