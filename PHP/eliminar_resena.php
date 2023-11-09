6b4f7aebc8f0b3a5b07fb1d7fc0ae35a4bead366
Daniel Maturana <matucristi@gmail.com>
2023-11-09 20:19:42 -0300

    procedimiento almacenado implementado

--- PHP/eliminar_resena.php
+++ PHP/eliminar_resena.php
@@ -8,28 +8,10 @@ $eliminado = false;
 if (isset($_SESSION['email'])) {
     $conn = Cconexion::ConexionBD();
 
-    $sql_delete = "DELETE FROM resenas
-                   WHERE id_receta = ? AND id_user = ?";
-    $stmt_delete = $conn->prepare($sql_delete);
-    $stmt_delete->execute([$id_receta, $id_usuario]);
-
-    $sql_select = "SELECT AVG(calificacion) AS promedio
-                   FROM resenas
-                   WHERE id_receta = ?";
-    $stmt_select = $conn->prepare($sql_select);
-    $stmt_select->execute([$id_receta]);
-
-    if ($stmt_select->rowCount() > 0) {
-        // Obtener el promedio de las calificaciones y actualizarlo
-        $row_promedio = $stmt_select->fetch();
-        $promedio = $row_promedio["promedio"];
-
-        $sql_update = "UPDATE recetas
-                       SET promedio_calificaciones = ?
-                       WHERE id_receta = ?";
-        $stmt_update = $conn->prepare($sql_update);
-        $stmt_update->execute([$promedio, $id_receta]);
-    }
+    // llamada a procedimiento almacenado
+    $sql_call ="CALL EliminarResena(?,?)";
+    $stmt = $conn->prepare($sql_call);
+    $stmt->execute([$id_usuario,$id_receta]);
 
     $eliminado = true;
     header("location: ver_resenas.php?eliminado=$eliminado"); 
