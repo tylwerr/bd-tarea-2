524da49af26c858c6580333b50ee24f4c0fedbd4
Xharless <carlosarevalo1702@hotmail.com>
2023-11-10 23:57:30 -0300

    atualizacion votacion semanal

--- PHP/votacion_semanal.php
+++ PHP/votacion_semanal.php
@@ -1,5 +1,8 @@
 <?php 
 include("top_bar.php");
+$sql = "SELECT * FROM recetas WHERE tipo_platillo = 'Plato de fondo' ORDER BY RAND() LIMIT 3 ";
+$resultado = $conn->query($sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +13,15 @@ include("top_bar.php");
     <title>Votaciones</title>
 
     <style>
+        .col-md-4 {
+            padding: 20px;
+            border-radius: 10px;
+        }
+
+        .card-img-top {
+            height: 200px;
+            object-fit: cover;
+        }
          .btn-back {
             color: #fff;
             background-color: #337ab7;
@@ -25,7 +37,22 @@ include("top_bar.php");
     <div style="margin: 10px;">
         <a href="principal.php" class="btn btn-back">Atrás</a>
     </div>
-
+    <h2 class="text-center my-5">Votacion Semanal</h2>
+    <div class="container">
+        <div class="row">
+            <?php while ($row = $resultado->fetch(PDO::FETCH_ASSOC)) { ?>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="<?php echo $row['url_imagen']; ?>" class="card-img-top img-fluid" alt="<?php echo $row['nombre_receta']; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $row['nombre_receta']; ?></h5>
+                        <a href="ver_receta.php?id_receta=<?php echo urlencode($row['id_receta']);?>&mensaje=" class="btn btn-primary">Ver</a>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
 
 
 
